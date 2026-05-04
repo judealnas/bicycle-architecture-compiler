4fee4cc9efdb2d97ec30a84bee502c228f27475b
judealnas <jude.alnas@duke.edu>
2026-05-04 19:03:05 -0400

    fix: switch to PathArchitecture for qubit operations

--- crates/bicycle_compiler/src/main.rs
+++ crates/bicycle_compiler/src/main.rs
@@ -150,12 +150,14 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     // Set the architecture based on the first operation
     let first_op = ops.peek();
     let architecture = if let Some(op) = first_op {
-        // PathArchitecture::for_qubits(op.basis().len())
-        FullArchitecture::for_qubits(op.basis().len())
+        PathArchitecture::for_qubits(op.basis().len())
+        // FullArchitecture::for_qubits(op.basis().len())
     } else {
         // No ops, may as well terminate now.
         return Ok(());
     };
+
+    dbg!(architecture);
 
     let compiled = ops.map(|op| op.compile(&architecture, &measurement_table, cli.accuracy));
 
